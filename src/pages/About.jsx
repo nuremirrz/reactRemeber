5f41c262beb4febb4a9e2b0fb2d10dc7cd74d0bf
Emirlan <nur.emirlan.02@gmail.com>
2022-07-22 20:42:22 +0600

    letsgo

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <h1>
+            Emirlan doesn't like this work     
+        </h1>
+    );
+};
+
+export default About;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,25 @@
+import React, { useContext } from 'react';
+import MyButton from '../components/UI/button/MyButton';
+import MyInput from '../components/UI/input/MyInput';
+import { AuthContext } from '../context';
+
+const Login = () => {
+    const {isAuth, setIsAuth} = useContext(AuthContext);
+    const login = event => {
+        event.preventDefault();
+        setIsAuth(true);
+        localStorage.setItem('auth', 'true')
+    }
+    return (
+        <div>
+            <h1>Page for login</h1>
+            <form onSubmit={login}>
+                <MyInput type="text" placeholder="Write your Login"/>
+                <MyInput type="password" placeholder="Write your password"/>
+                <MyButton>LogIn</MyButton>
+            </form>
+        </div>
+    );
+};
+
+export default Login;
